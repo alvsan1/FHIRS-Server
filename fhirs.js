30e3339e8c529bf7c24d957e54a2418d60113b21
alvsan1 <alv_sanchez@hotmail.com>
2018-09-11 15:58:52 -0300

    Schema View Ok V1

--- fhirs.js
+++ fhirs.js
@@ -5,17 +5,21 @@ const mongoose = require('mongoose')
 const restify = require('express-restify-mongoose')
 const app = express()
 const router = express.Router()
+const cors = require('cors');
 
 app.use(bodyParser.json())
 app.use(methodOverride())
+app.use(cors())
+
 
 
 
 // connect to database
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/fhirs', {
-  useMongoClient: true,
+  useNewUrlParser: true
 });
+//mongoose.connect('mongodb://localhost:27017/fhirs');
 
 
 Schema = mongoose.Schema;
@@ -50,6 +54,92 @@ function addModels(path){
           addConcept(path + items[i]);          
       }
   });
+
+  setTimeout(function()
+    {
+      fs.readdir(path, function(err, items) {
+       
+          for (var i=0; i<items.length; i++) {
+
+              //addConcept(path + items[i]);
+              addDefinitions(items[i].replace(/\.ttl/, ''))
+          }
+      });
+    },10000);
+
+}
+
+
+function addDefinitions(conceptName){
+
+  'use strict';
+
+  const fs = require('fs');  
+
+  let rawdata = fs.readFileSync("../FHIRS_Client/definitions/"+conceptName+'.json');  
+  let objJson = JSON.parse(rawdata);  
+  console.log("*************************************************");
+  console.log(conceptName);  
+  //console.log(objJson.definitions);  
+
+  for(var i in objJson.definitions){
+    var key = i;
+    console.log(i);
+    let relationJson = JSON.parse(fs.readFileSync("../FHIRS_Client/definitions/"+i+'.json'));
+    console.log(relationJson);
+    if (relationJson.definitions != undefined){
+      for (var definition in relationJson.definitions ){
+        console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        console.log(definition)
+        //let definitionJson = JSON.parse(fs.readFileSync("../FHIRS_Client/definitions/"+definition+'.json'));     
+            
+        objJson.definitions[definition] = relationJson.definitions[definition];
+
+
+/*
+        if (definitiosAux != undefined ){
+                
+        }
+        
+        objJson.definitions[i] = definitionJson;
+        
+        if (definitiosAux != undefined ){
+          for(var i in definitiosAux){
+            objJson.definitions[i] = definitiosAux[i];
+          }
+        }
+
+        */
+        //console.log(definitiosAux);
+
+      }
+      delete relationJson["definitions"];
+    }
+    objJson.definitions[i] = relationJson;
+    /*for(var j in val){
+        var sub_key = j;
+        var sub_val = val[j];
+        console.log(sub_key);
+    }*/
+  }
+
+  //console.log("****************addDefinitions**********************************");
+  //console.log(objJson);
+
+
+    'use strict';
+    let data = JSON.stringify(objJson);  
+    
+    fs.writeFileSync("../FHIRS_Client/definitions/"+conceptName+'.json', data);
+  //console.log(jsonFile);
+  
+
+
+  /*jsonFile = Object.assign(jsonFile, {title: conceptName, type: "object"})
+
+
+  
+*/
 }
 
 function addDataTypes(path){ 
@@ -77,6 +167,20 @@ function addDataTypes(path){
       }
   });
 
+
+  setTimeout(function()
+    {
+      fs.readdir(path, function(err, items) {
+       
+          for (var i=0; i<items.length; i++) {
+
+              //addConcept(path + items[i]);
+              addDefinitions(items[i].replace(/\.ttl/, ''))
+          }
+      });
+    },5000);
+
+
 }
 
 
@@ -97,8 +201,8 @@ function addConcept(path){
   let regexp = new RegExp("^.*\/(.*)\.ttl");
   let result = path.match(regexp);
   var conceptName = result[1];
-  console.log(path)
-  console.log(conceptName);
+  //console.log(path)
+  //console.log(conceptName);
 
   var modelo = require('mongoose').model(conceptName).schema;
 
@@ -108,11 +212,59 @@ function addConcept(path){
       console: false
   });
 
+  var jsonFile = {};
   rd.on('line', function(line) {
-      fhirsConceptTurtleToSchemaLine(conceptName, modelo, line);
+      let resultJson = fhirsConceptTurtleToSchemaLine(conceptName, modelo, line);
+      /*console.log((resultJson != undefined) ? resultJson : "Result null ");
+      console.log((resultJson != undefined) ? JSON.parce(resultJson) : "Result null ");
+      console.log((resultJson != undefined) ? resultJson.lenght !== undefined : "Result null ");*/
+      //console.log(resultJson.lenght !== undefined)
+      if (resultJson != null ){
+        if ( jsonFile.properties != undefined && jsonFile.properties.length != 0 ){
+    //      console.log(jsonFile);
+      //    console.log(Object.keys(jsonFile).lenght);
+          //console.log("****************hhhhhhhhhhhhhhhhhhhh      "+ conceptName +"   " + line + "**********************************");
+          Object.assign(jsonFile.properties, resultJson.properties)
+        }
+        else {
+          //console.log("****************kkkkkkkkkkkkkkkkkkkkkkkkk      "+ conceptName +"   " + line + "**********************************");
+          //console.log(resultJson);
+          //console.log(resultJson.length);
+          jsonFile.properties = resultJson.properties ;          
+        }
+
+        if (resultJson.definitions  != undefined ){
+          if (resultJson.definitions.length == 0 ){
+            
+          } 
+          ///////////REVISA SI YA TENIA UNA DEFINICIONES /////////////////////////////
+          if ( jsonFile.definitions != undefined ){
+            Object.assign(jsonFile.definitions , resultJson.definitions);
+          }else{
+            jsonFile.definitions = resultJson.definitions;
+          }
+          //console.log("****************" +"definitions" +"**********************************");          
+        }
+        //console.log("****************" +"definitions" +"**********************************");          
+        //console.log(jsonFile)
+      }
   });
 
-  var server = restify.serve(router, mongoose.model(conceptName, modelo), options)
+  rd.on('close', function() {
+    //console.log("****************hhhhhhhhhhhhhhhhhhhh**********************************");
+    //console.log(jsonFile);
+    jsonFile = Object.assign(jsonFile, {title: conceptName, type: "object"})
+
+
+    'use strict';
+    let data = JSON.stringify(jsonFile);  
+    fs.writeFileSync("../FHIRS_Client/definitions/"+conceptName+'.json', data);
+    //process.exit(0);
+
+    var server = restify.serve(router, mongoose.model(conceptName, modelo), options)
+
+  });
+ 
 
 }
 
@@ -124,8 +276,8 @@ function addDataType(path){
   let regexp = new RegExp("^.*\/(.*)\.ttl");
   let result = path.match(regexp);
   var dataTypeName = result[1];
-  console.log(path)
-  console.log(dataTypeName);
+  //console.log(path)
+  //console.log(dataTypeName);
 
   
   var modelo = require('mongoose').model(dataTypeName).schema;
@@ -135,98 +287,208 @@ function addDataType(path){
       output: process.stdout,
       console: false
   });
-
+  var jsonFile = {};
   rd.on('line', function(line) {
-      fhirsConceptTurtleToSchemaLine(dataTypeName, modelo, line);
+      let resultJson = fhirsConceptTurtleToSchemaLine(dataTypeName, modelo, line);
+      //jsonFile = Object.assign(jsonFile, resultJson)
+
+
+
+      if (resultJson != null ){
+        if ( jsonFile.properties != undefined && jsonFile.properties.length != 0 ){
+    //      console.log(jsonFile);
+      //    console.log(Object.keys(jsonFile).lenght);
+          //console.log("****************hhhhhhhhhhhhhhhhhhhh      "+ dataTypeName +"   " + line + "**********************************");
+          Object.assign(jsonFile.properties, resultJson.properties)
+        }
+        else {
+          //console.log("****************kkkkkkkkkkkkkkkkkkkkkkkkk      "+ dataTypeName +"   " + line + "**********************************");
+          //console.log(resultJson);
+          //console.log(resultJson.length);
+          jsonFile.properties = resultJson.properties ;
+        }
+
+        if (resultJson.definitions  != undefined ){
+          if (resultJson.definitions.length == 0 ){
+
+          } 
+          ///////////REVISAR SI YA TENIA UNA DEFINICION /////////////////////////////
+          if ( jsonFile.definitions != undefined ){
+            Object.assign(jsonFile.definitions , resultJson.definitions);
+          }else{
+            jsonFile.definitions = resultJson.definitions;
+          }
+          //console.log("****************" +"definitions" +"**********************************");          
+        }
+        //console.log("****************" +"definitions" +"**********************************");          
+        //console.log(jsonFile)
+        
+      }
+
+
+
+
+
+      //jsonFile.concat(resultJson);      
+  })
+
+  rd.on('close', function() {
+    //console.log("****************hhhhhhhhhhhhhhhhhhhh**********************************");
+    //console.log(jsonFile);
+    jsonFile = Object.assign(jsonFile, {title: dataTypeName, type: "object"})
+
+
+    'use strict';
+    let data = JSON.stringify(jsonFile);  
+    fs.writeFileSync("../FHIRS_Client/definitions/"+dataTypeName+'.json', data);
+    //process.exit(0);
+
+
+
   });
 
 }
 
 
 function fhirsConceptTurtleToSchemaLine(conceptName,schema, line){
-  console.log("#########fhirsConceptTurtleToSchemaLine###############");
-  console.log(conceptName);
+  //console.log("#########fhirsConceptTurtleToSchemaLine###############");
+  //console.log(conceptName);
 
 
   let regexp = new RegExp(".*fhir:"+conceptName+"\\.(.*) \\[ (.*) \\](, \\.\\.\\.|;) .*# (.*)?");
   let result = line.match(regexp);
   if (!(result == null || result == undefined)) {
-    console.log(result);
-    console.log(result[1]);
+    //console.log(result);
+    //console.log(result[1]);
     
 
 
     let parameter = result[1];
     let jsonObj = {};
+    let schemaJson = {};
     let valParameter = null;
+    let schemaParameter = null;
 
 
     switch (true) {
       case /^ *#.*/.test(result[0]):
-        console.log("• Matched Commment in code");
+        //console.log("• Matched Commment in code");
         break;
       case /code/.test(result[2]):
-        console.log("• Matched code DataType");
+        //console.log("• Matched code DataType");
         let codes = (result[4].match(/\d (.*)/)[1]).split(" | ");
         if ( codes.length > 1 ){
           if ( result[3] == ", ..."){
             valParameter = {type: [String],
                             enum: codes};
+            schemaParameter = {type: "array",
+                               itmes: {type: "string",
+                            enum: codes}};
           } else {
             valParameter = {type: String,
+                            enum: codes};
+            schemaParameter = {type: "string",
                             enum: codes};
           }
         }else{
             valParameter = {type: String};
+            schemaParameter = {type: "string"};
         }
         jsonObj[result[1]] = valParameter;
+        schemaJson["properties"] = {};
+        schemaJson["properties"][result[1]] = schemaParameter;
         break;
       case /string/.test(result[2]):
-        console.log("• Matched code string");
-        valParameter = {type: String};
+        //console.log("• Matched code string");
+        if ( result[3] == ", ..."){
+          valParameter = {type: [String]}
+          schemaParameter = {type: "array",
+                             itmes: {type: "string"
+                           }};
+        }else{
+          valParameter = {type: String};
+          schemaParameter = {type: "string"};
+        }
         jsonObj[result[1]] = valParameter;
+        schemaJson["properties"] = {};
+        schemaJson["properties"][result[1]] = schemaParameter;
         break;
       case /boolean/.test(result[2]):
-        console.log("• Matched code string");
-        valParameter = {type: Boolean};
+        //console.log("• Matched code string");
+        if ( result[3] == ", ..."){
+          valParameter = {type: Boolean};
+          schemaParameter = {type: "array",
+                             itmes: {type: "boolean"
+                           }};
+        }else{
+          valParameter = {type: Boolean};
+          schemaParameter = {type: "boolean"};
+        }
+        
         jsonObj[result[1]] = valParameter;
+        schemaJson["properties"] = {};
+        schemaJson["properties"][result[1]] = schemaParameter;
         break;
       case /dateTime/.test(result[2]):
-        console.log("• Matched code string");
+        //console.log("• Matched code string");
         valParameter = {type: Date};
+        schemaParameter = {type: "string", format: "date"}
+
         jsonObj[result[1]] = valParameter;
+        schemaJson["properties"] = {};
+        schemaJson["properties"][result[1]] = schemaParameter;
+
         break;        
       case /uri/.test(result[2]):
-        console.log("• Matched uri DataType");
+        //console.log("• Matched uri DataType");
         valParameter = {type: String, 
                         validate: require('mongoose-validators').isURL()};
         jsonObj[result[1]] = valParameter;
         break;
       case /Reference.*/.test(result[2]):
-        console.log("• Matched Reference");
+        //console.log("• Matched Reference");
         let objectReference = result[2].match((new RegExp("Reference.(.*).")))[1];
         valParameter = [{ type: Schema.Types.ObjectId, ref: objectReference }]
         jsonObj[result[1]] = [valParameter];
         break;       
       default:
-        console.log("• Didn't match first level");
+        //console.log("• Didn't match first level");
         valParameter = require('mongoose').model(result[2]).schema
         if ( valParameter == null ){
-          console.log("• Didn't match any test");
+          //console.log("• Didn't match any test");
           valParameter = String;
         } else {
-          console.log("• Is DataType Valid.");
-          jsonObj[result[1]] = valParameter;          
+          //console.log("• Is DataType Valid.");
+          //console.log(result[2]);
+          jsonObj[result[1]] = valParameter; 
+
+          schemaParameter = {$ref: "#/definitions/"+result[2], title: result[2]};
+          schemaJson["properties"] = {};
+          schemaJson["properties"][result[1]] = schemaParameter;
+
+          
+          schemaJson["definitions"] = {};
+          schemaJson["definitions"][result[2]] = {};         
+          
         }
         break;
     }
 
     if (jsonObj != {}){
-      console.log("Add parameter " + jsonObj);
+      //console.log("Add parameter " + jsonObj);
 
       let schemaVar = require('mongoose').model(conceptName).schema.add(jsonObj);
     }
 
+    //console.log(schemaJson);
+    //console.log(Object.keys(schemaJson).length );
+    if (Object.keys(schemaJson).length != 0 ) {
+        //console.log(schemaJson);
+        return schemaJson;  
+    }else{
+      return null;
+    }
+    
   }
 }
 
@@ -252,8 +514,21 @@ addModels(pathDefinitions);
 
 app.get('/', function (req, res, next) {
 
-  let schema = require('mongoose').model('Specimen').schema.paths;  
-  return res.json({ schema });
+  let schema = require('mongoose').model('Specimen').schema;
+  
+
+  var jsonSchemaGenerator = require('json-schema-generator'),
+    obj = { some: { object: true } },
+    schemaObj;
+ 
+  schemaObj = jsonSchemaGenerator(schema);
+
+  if ( schemaObj != null )
+    return res.json({ schema });
+  else
+    return res.json({error:"No existe el schema"})
+  
+
 
 /*
    res.send({ hello: 'world' });
@@ -271,7 +546,7 @@ app.get('/addParameter', function (req, res, next){
 
 app.use(router)
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log('Express server listening on port 3000')
 })
 
